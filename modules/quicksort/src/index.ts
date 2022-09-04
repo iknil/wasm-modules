b1d7e27d13f0d9b54e13ad2965c07a65bee32b83
iknil <lk.nil@outlook.com>
2022-09-04 01:43:15 +0000

    feat: 0.3.1

--- modules/quicksort/src/index.ts
+++ modules/quicksort/src/index.ts
@@ -1,4 +1,4 @@
-let wasm = require('../pkg/quicksort');
+let wasm = require('./libs/quicksort');
 
 module.exports = function quicksort(arr: number[], asc: boolean = true) {
     return wasm.quicksort(arr, asc);
